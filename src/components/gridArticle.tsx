aa9bf1c3fe69093b07d845ac9fe91575c16d619a
Andrian <fehizoroandriantsarafara@gmail.com>
2025-02-18 12:52:42 +0300

    feat : ajout de la fontionnalité recherche par nom

--- src/components/gridArticle.tsx
+++ src/components/gridArticle.tsx
@@ -4,10 +4,12 @@ import gridArticleProps from '../types/gridArticle'
 import ArticleCard from "./articleCard"
 export default function GridArticle (articles:gridArticleProps) {
 
-    const {filterCategory} = useFilterStore()
+    const {searchQuerry,filterCategory} = useFilterStore()
     
     let filteredArticle = articles.listArticle.filter(article => 
-        (filterCategory==="all" || article.categorie === filterCategory)
+        ( article.nom.includes(searchQuerry) 
+        && 
+        (filterCategory==="all" || article.categorie === filterCategory))
     )
 
     return <div className='flex-1 grid gap-4 place-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4'>
--- src/stores/filterStore.tsx
+++ src/stores/filterStore.tsx
@@ -1,14 +1,18 @@
 import {create} from 'zustand'
 
 interface State {
+    searchQuerry : string,
     filterCategory : string
 }
 
 interface Action {
+    setSearchQuerry : (searched: string) => void,
     setFilterCategory : (filter: string) => void
 }
 
 export const useFilterStore = create<State & Action>((set)=>({
+    searchQuerry:'',
     filterCategory:'',
+    setSearchQuerry:(searched: string) => set((state)=>({...state,searchQuerry:searched})),
     setFilterCategory:(filter: string) => set((state)=>({...state,filterCategory:filter})),
 }))
