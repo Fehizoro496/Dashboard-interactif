edd52a7ee3b5110687694a460b14e944b143c86d
Andrian <fehizoroandriantsarafara@gmail.com>
2025-02-23 00:31:21 +0300

    fix: amélioration de la mise en page du modal de modification d'article

--- src/components/updateModal.tsx
+++ src/components/updateModal.tsx
@@ -70,57 +70,59 @@ const UpdateArticleModal = () => {
 
     return (
         <Dialog open={isDialogOpen && typeDialog==="modify"} onOpenChange={handleClose}>
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Modify an Article</DialogTitle>
-                    <DialogDescription/>
-                </DialogHeader>
-                <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-                    <div className="space-y-1">
-                        <Label htmlFor="title">Title</Label>
-                        <Input id="title" {...register('title')} required />
-                        {errors.title && <p className="text-red-500 text-sm">{errors.title.message}</p>}
-                    </div>
-                    <div className="space-y-1">
-                        <Label htmlFor="price">Price</Label>
-                        <Input id="price" type="number" step={0.01} {...register('price', { valueAsNumber: true })} required />
-                        {errors.price && <p className="text-red-500 text-sm">{errors.price.message}</p>}
-                    </div>
-                    <div className="space-y-1">
-                        <Label htmlFor="category">Category</Label>
-                        <Input id="category" {...register('category')} required />
-                        {errors.category && <p className="text-red-500 text-sm">{errors.category.message}</p>}
-                    </div>
-                    <div className="grid grid-cols-2 gap-4">
-                        <div className="space-y-1">
-                            <Label htmlFor="image">Image URL</Label>
-                            <Input id="image" {...register('image')} required />
-                            {errors.image && <p className="text-red-500 text-sm">{errors.image.message}</p>}
+            <DialogContent className="max-w-[425px] md:max-w-[600px]">
+            <DialogHeader>
+                <DialogTitle>Modify an Article</DialogTitle>
+                <DialogDescription/>
+            </DialogHeader>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                <div className="flex flex-col sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center max-h-[450px] overflow-y-scroll md:max-h-full">
+                    {imageUrl && (
+                        <div className="relative aspect-square col-span-1">
+                        <Image 
+                            src={imageUrl}
+                            alt="Preview"
+                            fill
+                            className="object-contain"
+                            unoptimized
+                        />
                         </div>
-                        {imageUrl && (
-                            <div className="relative aspect-square">
-                                <Image 
-                                    src={imageUrl}
-                                    alt="Preview"
-                                    fill
-                                    className="object-contain"
-                                    unoptimized
-                                />
-                            </div>
-                        )}
+                    )}
+                <div className="space-y-4 col-span-2">
+                    <div className="space-y-1">
+                    <Label htmlFor="title">Title</Label>
+                    <Input id="title" {...register('title')} required />
+                    {errors.title && <p className="text-red-500 text-sm">{errors.title.message}</p>}
                     </div>
                     <div className="space-y-1">
-                        <Label htmlFor="description">Description</Label>
-                        <Textarea id="description" {...register('description')} required />
-                        {errors.description && <p className="text-red-500 text-xs">{errors.description.message}</p>}
+                    <Label htmlFor="price">Price</Label>
+                    <Input id="price" type="number" step={0.01} {...register('price', { valueAsNumber: true })} required />
+                    {errors.price && <p className="text-red-500 text-sm">{errors.price.message}</p>}
                     </div>
-                    <div className="flex justify-end gap-2">
-                        <Button type="button" variant="outline" onClick={handleClose}>
-                            Cancel
-                        </Button>
-                        <Button type="submit">Modify</Button>
+                    <div className="space-y-1">
+                    <Label htmlFor="category">Category</Label>
+                    <Input id="category" {...register('category')} required />
+                    {errors.category && <p className="text-red-500 text-xs">{errors.category.message}</p>}
                     </div>
-                </form>
+                    <div className="space-y-1">
+                    <Label htmlFor="image">Image URL</Label>
+                    <Input id="image" {...register('image')} required />
+                    {errors.image && <p className="text-red-500 text-xs">{errors.image.message}</p>}
+                    </div>
+                    <div className="space-y-1">
+                    <Label htmlFor="description">Description</Label>
+                    <Textarea id="description" {...register('description')} required />
+                    {errors.description && <p className="text-red-500 text-xs">{errors.description.message}</p>}
+                    </div>
+                </div>
+                </div>
+                <div className="flex justify-end gap-2">
+                <Button type="button" variant="outline" onClick={handleClose}>
+                    Cancel
+                </Button>
+                <Button type="submit">Modify</Button>
+                </div>
+            </form>
             </DialogContent>
         </Dialog>
     );
