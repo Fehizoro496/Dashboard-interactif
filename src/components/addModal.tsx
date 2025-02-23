2ce8b7e0f32ea703e6ea2e180e886043b07cfbdf
Andrian <fehizoroandriantsarafara@gmail.com>
2025-02-24 00:46:45 +0300

    fix: amélioration de la responsivité des modales d'ajout et de modification d'article

--- src/components/addModal.tsx
+++ src/components/addModal.tsx
@@ -55,61 +55,61 @@ const AddArticleModal = () => {
 
     return (
         <Dialog open={isOpen} onOpenChange={toogleIsOpen}>
-            <DialogContent className="max-w-[425px] md:max-w-[600px]">
-                <DialogHeader>
-                    <DialogTitle>Add New Article</DialogTitle>
-                    <DialogDescription/>
-                </DialogHeader>
-                <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-                    <div className="flex flex-col sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center max-h-[450px] overflow-y-scroll md:max-h-full">
-                        <div className="relative aspect-square col-span-1">
-                            {imageUrl ? (
-                                <Image 
-                                    src={imageUrl}
-                                    alt="Preview"
-                                    fill
-                                    className="object-contain"
-                                    unoptimized
-                                />
-                            ) : (
-                                <ImagePlaceholder />
-                            )}
-                        </div>
-                        <div className="space-y-4 col-span-2">
-                            <div className="space-y-1">
-                                <Label htmlFor="title">Title</Label>
-                                <Input id="title" {...register('title')} required />
-                                {errors.title && <p className="text-red-500 text-sm">{errors.title.message}</p>}
-                            </div>
-                            <div className="space-y-1">
-                                <Label htmlFor="price">Price</Label>
-                                <Input id="price" type="number" step={0.01} {...register('price', { valueAsNumber: true })} required />
-                                {errors.price && <p className="text-red-500 text-sm">{errors.price.message}</p>}
-                            </div>
-                            <div className="space-y-1">
-                                <Label htmlFor="category">Category</Label>
-                                <Input id="category" {...register('category')} required />
-                                {errors.category && <p className="text-red-500 text-sm">{errors.category.message}</p>}
-                            </div>
-                            <div className="space-y-1">
-                                <Label htmlFor="image">Image URL</Label>
-                                <Input id="image" {...register('image')} required />
-                                {errors.image && <p className="text-red-500 text-sm">{errors.image.message}</p>}
-                            </div>
-                            <div className="space-y-1">
-                                <Label htmlFor="description">Description</Label>
-                                <Textarea id="description" {...register('description')} required />
-                                {errors.description && <p className="text-red-500 text-xs">{errors.description.message}</p>}
-                            </div>
-                        </div>
+            <DialogContent className="max-w-[90vw] md:max-w-[600px]">
+            <DialogHeader>
+                <DialogTitle>Add New Article</DialogTitle>
+                <DialogDescription />
+            </DialogHeader>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                <div className="flex flex-col sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center max-h-[450px] overflow-y-auto md:max-h-full">
+                <div className="relative aspect-square col-span-1">
+                    {imageUrl ? (
+                    <Image
+                        src={imageUrl}
+                        alt="Preview"
+                        fill
+                        className="object-contain"
+                        unoptimized
+                    />
+                    ) : (
+                    <ImagePlaceholder />
+                    )}
+                </div>
+                <div className="space-y-4 col-span-2">
+                    <div className="space-y-1">
+                    <Label htmlFor="title">Title</Label>
+                    <Input id="title" {...register('title')} required />
+                    {errors.title && <p className="text-red-500 text-sm">{errors.title.message}</p>}
                     </div>
-                    <div className="flex justify-end gap-2">
-                        <Button type="button" variant="outline" onClick={toogleIsOpen}>
-                            Cancel
-                        </Button>
-                        <Button type="submit">Add Article</Button>
+                    <div className="space-y-1">
+                    <Label htmlFor="price">Price</Label>
+                    <Input id="price" type="number" step={0.01} {...register('price', { valueAsNumber: true })} required />
+                    {errors.price && <p className="text-red-500 text-sm">{errors.price.message}</p>}
                     </div>
-                </form>
+                    <div className="space-y-1">
+                    <Label htmlFor="category">Category</Label>
+                    <Input id="category" {...register('category')} required />
+                    {errors.category && <p className="text-red-500 text-sm">{errors.category.message}</p>}
+                    </div>
+                    <div className="space-y-1">
+                    <Label htmlFor="image">Image URL</Label>
+                    <Input id="image" {...register('image')} required />
+                    {errors.image && <p className="text-red-500 text-sm">{errors.image.message}</p>}
+                    </div>
+                    <div className="space-y-1">
+                    <Label htmlFor="description">Description</Label>
+                    <Textarea id="description" {...register('description')} required />
+                    {errors.description && <p className="text-red-500 text-xs">{errors.description.message}</p>}
+                    </div>
+                </div>
+                </div>
+                <div className="flex justify-end gap-2">
+                <Button type="button" variant="outline" onClick={toogleIsOpen}>
+                    Cancel
+                </Button>
+                <Button type="submit">Add Article</Button>
+                </div>
+            </form>
             </DialogContent>
         </Dialog>
     );
